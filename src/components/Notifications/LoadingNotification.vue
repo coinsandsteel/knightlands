317aad0d86abb0296979c4aabe37f8d84d17c490
coinsandsteeldev <admin@coinsandsteel.com>
2020-08-08 01:05:56 +0700

    fix: loading overlay didn't block user inputs

--- src/components/Notifications/LoadingNotification.vue
+++ src/components/Notifications/LoadingNotification.vue
@@ -1,13 +1,15 @@
 <template>
-    <notifications group="loading" position="top" width="100%">
-      <template slot="body" slot-scope="props">
-        <div class="flex flex-center loading padding-2">
+  <notifications group="loading" position="top" width="100%" height="100%">
+    <template slot="body" slot-scope="props">
+      <div class="full-height">
+        <div class="flex flex-center loading padding-2 relative">
           <div class="bg-color"></div>
           <GridLoader color="#fde648" :size="15" margin="1px" />
           <span class="font-size-30 relative margin-left-2">{{$t("loading")}}</span>
         </div>
-      </template>
-    </notifications>
+      </div>
+    </template>
+  </notifications>
 </template>
 
 <style lang="less" scoped>
@@ -19,5 +21,9 @@
   right: 0;
   background-color: #210028;
   opacity: 0.75;
+}
+
+.full-height {
+  height: 100vh;
 }
 </style>
