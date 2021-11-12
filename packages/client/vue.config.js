f8ce05bfa511480c39a32e1e5debc9304b0d0a06
uniwertz <uniwertz@gmail.com>
2021-11-12 20:00:57 +0400

    chg: Sync project version after build except dev

--- packages/client/vue.config.js
+++ packages/client/vue.config.js
@@ -1,7 +1,7 @@
 let optimization;
 let plugins = [];
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV !== "dev") {
   const WebpackShellPlugin = require('webpack-shell-plugin');
   plugins.push(
     new WebpackShellPlugin({
