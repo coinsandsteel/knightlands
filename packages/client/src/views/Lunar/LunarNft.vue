6374f3b105fb5af6b48d44ab13c0851abe1c2662
uniwertz <uniwertz@gmail.com>
2022-01-20 22:21:37 +0400

    chg: NFT page

--- packages/client/src/views/Lunar/LunarNft.vue
+++ packages/client/src/views/Lunar/LunarNft.vue
@@ -1,14 +1,230 @@
 <template>
   <div class="screen-content">
-    Nft
+    <div class="full-flex dummy-height width-100">
+      <div
+        class="width-100 height-100 dummy-height flex flex-column flex-no-wrap"
+      >
+        <div class="inv-root dummy-height full-flex width-100 height-100">
+          <div v-bar class="center width-100 height-100 dummy-height">
+            <div
+              v-for="(recipe, recipeIndex) in recipes"
+              :key="`recipe-${recipeIndex}`"
+              class="margin-bottom-5"
+            >
+              <div
+                class="recipe-name font-size-25 text-align-center padding-top-2 padding-bottom-2 capitalize"
+                :key="`recipe-name-${recipeIndex}`"
+              >
+                <div class="flex flex-center flex-column">
+                  <div class="margin-bottom-3">
+                    {{ recipe.name }}
+                  </div>
+                  <div class="margin-bottom-2">
+                    <Loot
+                      :key="`recipe-result-${recipeIndex}`"
+                      :item="recipe.id"
+                      :inventory="false"
+                      :quantity="recipe.resultItemQuantity"
+                      :itemSlotClasses="
+                        recipe && recipe.itemSlotClasses
+                          ? recipe.itemSlotClasses
+                          : null
+                      "
+                      :iconClasses="
+                        recipe && recipe.iconClasses ? recipe.iconClasses : null
+                      "
+                    >
+                      <div
+                        style="position: absolute; top: 0; left: 50%; transform: translate(-50%, -100%); font-size: 10px; color: red;"
+                      >
+                        {{ recipe && recipe.name ? recipe.name : null }}
+                      </div>
+                    </Loot>
+                  </div>
+                  <div class="text-center" :key="`craft-btn-${recipeIndex}`">
+                    <CustomButton
+                      :disabled="!recipe.canCraft"
+                      type="green"
+                      class="btn-combine inline-block uppercase padding-left-2"
+                      @click="craftHandler(recipe.recipeId)"
+                    >
+                      {{ $t("btn-combine") }}
+                    </CustomButton>
+                  </div>
+                </div>
+              </div>
+
+              <div
+                :key="`recipe-ingredients-${recipeIndex}`"
+                class="recipe-ingredients width-100 dummy-height inventory-container margin-top-2 margin-bottom-3 padding-left-1 padding-right-1"
+              >
+                <Loot
+                  v-for="(item, itemIndex) in recipe.ingredients"
+                  :id="`i-${item.id}`"
+                  :item="item.id"
+                  :key="itemIndex"
+                  :inventory="false"
+                  :itemSlotClasses="
+                    item && item.itemSlotClasses ? item.itemSlotClasses : null
+                  "
+                  :iconClasses="
+                    item && item.iconClasses ? item.iconClasses : null
+                  "
+                  :class="{
+                    'opacity-50': !item.quantity
+                  }"
+                >
+                  <div
+                    style="position: absolute; top: 0; left: 50%; transform: translate(-50%, -100%); font-size: 10px; color: red;"
+                  >
+                    {{ item && item.name ? item.name : null }}
+                  </div>
+                </Loot>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import NetworkRequestErrorMixin from "@/components/NetworkRequestErrorMixin.vue";
+import ActivityMixin from "@/components/ActivityMixin.vue";
+import Loot from "@/components/Loot.vue";
+import CustomButton from "@/components/Button.vue";
+
+import {
+  ITEM_RARITY_EXPERT,
+  ITEM_RARITY_NFT,
+  RARITY_CLASS_MAP
+} from "@/../../knightlands-shared/lunar";
+import recipes from "@/crafting_recipes.json";
+
+const nftRecipes = Object.values(recipes).filter(
+  ({ category, ingridients }) =>
+    category === "lunar" && ingridients.length >= 10
+);
+
 export default {
-  components: {},
+  components: {
+    Loot,
+    CustomButton
+  },
+  mixins: [ActivityMixin, NetworkRequestErrorMixin],
   data() {
     return {};
+  },
+  computed: {
+    recipes() {
+      return nftRecipes.map(this.generateRecipeItem.bind(this));
+    },
+    nfts() {
+      return this.$game.inventory.items.filter(({ template }) => {
+        const info = this.$game.itemsDB.getTemplate(template);
+        return info.type === "lunarResource" && info.rarity === ITEM_RARITY_NFT;
+      });
+    },
+    items() {
+      let items = this.$game.inventory.items.filter(({ template }) => {
+        const info = this.$game.itemsDB.getTemplate(template);
+        return (
+          info.type === "lunarResource" && info.rarity === ITEM_RARITY_EXPERT
+        );
+      });
+      let i = 0;
+      const length = items.length;
+      let filteredItems = [];
+      for (; i < length; ++i) {
+        const item = items[i];
+        const rarity = this.$game.itemsDB.getRarity(item.template);
+        let lanternIds = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2];
+        let idString = item.id.toString();
+        let lanternId = lanternIds[idString[idString.length - 1]];
+        let icon = "basic_lantern" + lanternId;
+        filteredItems.push({
+          ...item,
+          iconClasses: `${RARITY_CLASS_MAP[rarity]} ${icon}`,
+          itemSlotClasses: "lunar-lantern-slot",
+          isCustomElement: true
+        });
+      }
+      return filteredItems;
+    }
+  },
+
+  methods: {
+    generateRecipeItem(recipe) {
+      const achievementInfo = this.$game.itemsDB.getTemplate(recipe.resultItem);
+      const achievement = {
+        ...achievementInfo,
+        resultItemQuantity: this.$game.inventory.getItemsCountByTemplate(recipe.resultItem),
+        recipeId: recipe.id,
+        isCustomElement: true,
+        itemSlotClasses: "lunar-lantern-slot",
+        iconClasses: achievementInfo.icon,
+        name: this.$t(achievementInfo.caption)
+      };
+
+      const ingredients = [];
+      for (let i = 0; i < recipe.ingridients.length; i++) {
+        const ingredientId = recipe.ingridients[i].itemId;
+        const ingredientInfo = this.$game.itemsDB.getTemplate(ingredientId);
+        let lanternIds = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2];
+        let idString = ingredientInfo.id.toString();
+        let lanternId = lanternIds[idString[idString.length - 1]];
+        let icon = "basic_lantern" + lanternId;
+
+        const ingredient = {
+          ...ingredientInfo,
+          isCustomElement: true,
+          itemSlotClasses: "lunar-lantern-slot",
+          iconClasses: `${RARITY_CLASS_MAP[ingredientInfo.rarity]} ${icon}`,
+          name: this.$t(ingredientInfo.caption),
+          ingredientCount: 1
+        };
+        const ownedItem = this.items.find(
+          ({ template }) => template === ingredient.id
+        );
+        if (ownedItem) {
+          ingredient.quantity = ownedItem.count;
+        } else {
+          ingredient.quantity = 0;
+        }
+        ingredients.push(ingredient);
+      }
+
+      achievement.ingredients = ingredients;
+      achievement.canCraft = ingredients.every(
+        ({ quantity, ingredientCount }) => quantity >= ingredientCount
+      );
+
+      return achievement;
+    },
+    async craftHandler(recipeId) {
+      if (this.hasCrafted) {
+        return;
+      }
+      await this.performRequestNoCatch(
+        this.$store.dispatch("lunar/craft", { recipeId })
+      );
+    }
   }
 };
 </script>
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.v-bar-fix {
+  & > div {
+    overflow: auto !important;
+  }
+}
+.recipe-ingredients {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(8rem, 1fr));
+  justify-items: center;
+  grid-gap: 2rem;
+}
+.recipe-name {
+  background: rgba(0, 0, 0, 0.5);
+}
+</style>
