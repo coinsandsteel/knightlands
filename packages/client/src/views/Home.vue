693d6ade1b5c3d8f312a877cf4bef90fcf12be7b
imyourm8 <imyourm9@gmail.com>
2022-02-07 23:26:40 +0700

    chg: remove Lunar event icon

--- packages/client/src/views/Home.vue
+++ packages/client/src/views/Home.vue
@@ -20,13 +20,13 @@
 
                 <template v-slot:marker> <DailyTasksMarker /> </template
               ></MenuIcon>
-              <MenuIcon
+              <!-- <MenuIcon
                 icon="skewed_icon_xmas"
                 :to="{ path: '/lunar' }"
                 :append="true"
                 id="lunar"
                 >{{ $t("btn-evt-lunar") }}
-              </MenuIcon>
+              </MenuIcon> -->
             </MenuIconRow>
 
             <MenuIconRow>
