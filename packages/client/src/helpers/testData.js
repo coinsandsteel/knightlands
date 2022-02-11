8de959dc53e5464fa9cfbb509a4a40373319c0e6
uniwertz <uniwertz@gmail.com>
2022-02-11 17:41:22 +0400

    chg: Response structure

--- packages/client/src/helpers/testData.js
+++ packages/client/src/helpers/testData.js
@@ -59,11 +59,9 @@ export const c2 = {
     { _id: "0", unitClass: march.UNIT_CLASS_ENEMY, hp: 4 },
     { _id: "1", unitClass: march.UNIT_CLASS_ENEMY, hp: 5 },
     { _id: "2", unitClass: march.UNIT_CLASS_CHEST, hp: 5 },
-
     { _id: "3", unitClass: march.UNIT_CLASS_BOW, hp: 5 },
     { _id: "4", unitClass: march.UNIT_CLASS_PET, hp: 8, maxHp: 10 },
     { _id: "5", unitClass: march.UNIT_CLASS_ENEMY_BOSS, hp: 8 },
-
     { _id: "6", unitClass: march.UNIT_CLASS_TRAP, hp: 3, opened: true },
     { _id: "7", unitClass: march.UNIT_CLASS_HP, hp: 1 },
     { _id: "8", unitClass: march.UNIT_CLASS_GOLD, hp: 4 }
@@ -71,17 +69,15 @@ export const c2 = {
   response: [
     {
       cards: [
-        { _id: "0", unitClass: march.UNIT_CLASS_ENEMY, hp: 4 },
-        { _id: "1", unitClass: march.UNIT_CLASS_ENEMY, hp: 5 },
-        { _id: "2", unitClass: march.UNIT_CLASS_CHEST, hp: 5 },
-
-        { _id: "4", unitClass: march.UNIT_CLASS_PET, hp: 8, maxHp: 10 },
-        { _id: "5", unitClass: march.UNIT_CLASS_ENEMY_BOSS, hp: 8 },
+        null,
+        null,
+        null,
+        { _id: "4" },
+        { _id: "5" },
         { _id: "9", unitClass: march.UNIT_CLASS_GOLD, hp: 5 },
-
-        { _id: "6", unitClass: march.UNIT_CLASS_TRAP, hp: 3, opened: true },
-        { _id: "7", unitClass: march.UNIT_CLASS_HP, hp: 1 },
-        { _id: "8", unitClass: march.UNIT_CLASS_GOLD, hp: 4 }
+        null,
+        null,
+        null
       ],
       effect: {
         unitClass: march.UNIT_CLASS_BOW,
@@ -92,16 +88,14 @@ export const c2 = {
     {
       cards: [
         { _id: "10", unitClass: march.UNIT_CLASS_GOLD, hp: 4 },
-        { _id: "1", unitClass: march.UNIT_CLASS_ENEMY, hp: 5 },
-        { _id: "2", unitClass: march.UNIT_CLASS_CHEST, hp: 5 },
-
-        { _id: "4", unitClass: march.UNIT_CLASS_PET, hp: 8, maxHp: 10 },
-        { _id: "5", unitClass: march.UNIT_CLASS_ENEMY_BOSS, hp: 3 },
-        { _id: "9", unitClass: march.UNIT_CLASS_GOLD, hp: 5 },
-
+        null,
+        null,
+        null,
+        { _id: "5", hp: 3 },
+        null,
         { _id: "11", unitClass: march.UNIT_CLASS_GOLD, hp: 3 },
-        { _id: "7", unitClass: march.UNIT_CLASS_HP, hp: 1 },
-        { _id: "8", unitClass: march.UNIT_CLASS_GOLD, hp: 4 }
+        null,
+        null
       ]
     }
   ]
