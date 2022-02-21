b18bdde608b054119fcc997731e3e29eb06acb47
Phuc Le <lehuuphucaptech@gmail.com>
2022-02-21 20:43:27 +0700

    pet details

--- packages/client/src/views/March/MarchPetDetails.vue
+++ packages/client/src/views/March/MarchPetDetails.vue
@@ -25,7 +25,7 @@
       </div> -->
       <div>
         <div
-          class="march-pet-element-wrapper flex flex-no-wrap flex-justify-center padding-top-2 padding-bottom-2"
+          class="march-pet-element-wrapper flex flex-no-wrap flex-justify-center padding-top-2 padding-bottom-3"
         >
           <MarchPetElement
             class="march-pet-element-details"
@@ -33,20 +33,65 @@
             :applyOpacity="false"
           />
         </div>
-        <div>
-          abilities
+        <div
+          class="march-pet-abilities-container font-size-22 margin-top-4 margin-bottom-2"
+        >
+          <div class="march-pet-abilities">
+            <template v-for="(ability, abilityIndex) in abilities">
+              <div
+                v-if="ability.name"
+                :key="'icon-' + ability.name"
+                class="march-pet-ability-wrapper relative"
+                :class="{
+                  'march-pet-ability-wrapper--locked': abilityIndex >= pet.level
+                }"
+              >
+                <div
+                  :class="[
+                    `march-pet-ability march-pet-ability-${
+                      pet.petClass
+                    }-${abilityIndex + 1}`
+                  ]"
+                ></div>
+              </div>
+              <div
+                v-if="ability.name"
+                :key="'name-' + ability.name"
+                class=" text-align-left"
+              >
+                {{
+                  abilityIndex >= pet.level
+                    ? "Level up to unlock this ability???"
+                    : ability.name
+                }}
+              </div>
+            </template>
+          </div>
         </div>
       </div>
     </template>
   </UserDialog>
 </template>
 <script>
+import * as march from "@/../../knightlands-shared/march";
 import MarchPetElement from "@/views/March/MarchPetElement.vue";
 export default {
   components: { MarchPetElement },
   props: ["pet"],
   data() {
     return {};
+  },
+  computed: {
+    abilities() {
+      if (!this.pet) {
+        return;
+      }
+
+      return march.PET_ABILITIES[this.pet.petClass - 1].slice(
+        0,
+        3 || this.pet.level
+      );
+    }
   },
   methods: {
     close() {
@@ -68,5 +113,43 @@ export default {
 .march-pet-element-details {
   width: 200px;
   max-width: 40%;
+}
+.march-pet-abilities-container {
+}
+.march-pet-abilities {
+  display: inline-grid;
+  grid-template-columns: 4rem 1fr;
+  grid-gap: 2rem;
+  max-width: 240px;
+  align-items: center;
+}
+.march-pet-ability-wrapper {
+  width: 4rem;
+  height: 4rem;
+}
+.march-pet-ability {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-size: 100%;
+  background-repeat: none;
+  position: relative;
+}
+.march-pet-ability-wrapper--locked {
+  .march-pet-ability {
+    opacity: 0.5;
+  }
+  &::after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 2rem;
+    height: 2rem;
+    transform: translate(-50%, -50%);
+    background-image: url("/images/march/icon_locked.png");
+    background-size: 100%;
+    background-repeat: none;
+  }
 }
 </style>
