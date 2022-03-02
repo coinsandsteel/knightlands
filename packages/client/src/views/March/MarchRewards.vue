c0c6d2dabed476b6e3b208c36218fbca9cc19c86
Phuc Le <lehuuphucaptech@gmail.com>
2022-03-02 17:50:27 +0700

    Update march rewards

--- packages/client/src/views/March/MarchRewards.vue
+++ packages/client/src/views/March/MarchRewards.vue
@@ -2,69 +2,68 @@
   <div class="font-size-22 text-center">
     <div class="">March rewards</div>
 
-    <div class="flex flex-no-wrap flex-center margin-top-2">
-      <div class="margin-right-2">Reward 1:</div>
-      <Loot :id="`i-${reward1.template}`" :item="reward1" :inventory="true" />
-    </div>
-    <div class="flex flex-no-wrap flex-center margin-top-2">
-      <div class="margin-right-2">Reward 2:</div>
-      <Loot :id="`i-${reward2.template}`" :item="reward2" :inventory="true" />
-    </div>
-    <div class="flex flex-no-wrap flex-center margin-top-2">
-      <div class="margin-right-2">Reward 3:</div>
-      <Loot :id="`i-${reward3.template}`" :item="reward3" :inventory="true" />
-    </div>
+    <template v-for="(reward, index) in rewards">
+      <div class="flex flex-no-wrap flex-center margin-top-2" :key="index">
+        <div class="margin-right-2">Reward {{ index + 1 }}:</div>
+        <Loot
+          :id="`i-${reward.template}-${reward.petClass}`"
+          :item="reward"
+          :inventory="true"
+          class="march-reward"
+          :class="`march-reward--${reward.petClass}`"
+        />
+      </div>
+    </template>
   </div>
 </template>
 <script>
+import { mapGetters } from "vuex";
 import Loot from "@/components/Loot.vue";
 
 export default {
   components: { Loot },
   computed: {
-    reward1() {
-      return {
-        id: 4260,
-        template: 3448,
-        count: 1,
+    ...mapGetters("march", ["pets"]),
+    rewards() {
+      const item = {
+        template: 1928,
         level: 1,
         exp: 0,
         equipped: false,
         breakLimit: 0,
         unique: false,
-        index: 8
+        rarity: "common"
       };
-    },
-    reward2() {
-      return {
-        id: 4252,
-        template: 3450,
-        count: 1,
-        level: 1,
-        exp: 0,
-        equipped: false,
-        breakLimit: 0,
-        unique: false,
-        index: 13
-      };
-    },
-    reward3() {
-      return {
-        id: 3006,
-        template: 2677,
-        count: 1,
-        level: 48,
-        exp: 90,
-        equipped: false,
-        breakLimit: 0,
-        unique: true,
-        rarity: "legendary",
-        element: "physical",
-        locked: false,
-        index: 20
-      };
+      const result = [];
+      this.pets.forEach(pet => {
+        if (pet.unlocked === true && pet.level === 3) {
+          result.push({ ...item, petClass: pet.petClass });
+        }
+      });
+
+      return result;
     }
   }
 };
 </script>
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.march-reward::v-deep .icon {
+  background-position: center;
+  background-size: 100%;
+}
+.march-reward--1::v-deep .icon {
+  background-image: url("/images/march/rewards/pet1nft.png");
+}
+.march-reward--2::v-deep .icon {
+  background-image: url("/images/march/rewards/pet2nft.png");
+}
+.march-reward--3::v-deep .icon {
+  background-image: url("/images/march/rewards/pet3nft.png");
+}
+.march-reward--4::v-deep .icon {
+  background-image: url("/images/march/rewards/pet4nft.png");
+}
+.march-reward--5::v-deep .icon {
+  background-image: url("/images/march/rewards/pet5nft.png");
+}
+</style>
