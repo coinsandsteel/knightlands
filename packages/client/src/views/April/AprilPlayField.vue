8bbdff95ee7e2ce79c3eb8f4feefad6dfacf7cd1
tran.duc <tranduc2702@gmail.com>
2022-03-18 14:03:28 +0700

    fix: cardId is string

--- packages/client/src/views/April/AprilPlayField.vue
+++ packages/client/src/views/April/AprilPlayField.vue
@@ -389,7 +389,7 @@ export default {
         return;
       }
       this.$store.dispatch("april/move", {
-        cardId: this.selectedCardId,
+        cardId: this.selectedCardId.toString(),
         index: cellIndex
       });
     }
