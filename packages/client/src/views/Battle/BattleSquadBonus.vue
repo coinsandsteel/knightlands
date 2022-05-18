f6ffa97f333e0813b0f39b1d77e46593d67d1e4a
Phuc Le <lehuuphucaptech@gmail.com>
2022-05-18 18:18:32 +0700

    chg: squad bonus

--- packages/client/src/views/Battle/BattleSquadBonus.vue
+++ packages/client/src/views/Battle/BattleSquadBonus.vue
@@ -1,11 +1,73 @@
 <template>
-  <div>Battle squad bonus</div>
+  <div class="font-size-22">
+    <div>
+      <div v-for="item in items" :key="item.user.id" class="margin-bottom-4">
+        <div>
+          Name
+        </div>
+        <div class="battle-unit-list">
+          <BattleUnit v-for="unit in item.units" :key="unit.id" />
+        </div>
+        <div>
+          <CustomButton
+            type="yellow"
+            width="20rem"
+            class="inline-block"
+            @click="showDetailsHandler"
+            >{{ $t("???") }}</CustomButton
+          >
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+import { create } from "vue-modal-dialogs";
+import BattleUnit from "@/views/Battle/BattleUnit.vue";
+import BattleSquadBonusDetails from "@/views/Battle/BattleSquadBonusDetails.vue";
 export default {
+  components: {
+    BattleUnit
+  },
   data() {
     return {};
+  },
+  computed: {
+    items() {
+      const result = [];
+      result.push({
+        user: { id: 1 },
+        units: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }]
+      });
+      result.push({
+        user: { id: 2 },
+        units: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }]
+      });
+      result.push({
+        user: { id: 3 },
+        units: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }]
+      });
+      return result;
+    }
+  },
+  methods: {
+    unitClickHandler() {},
+    async showDetailsHandler() {
+      const show = create(BattleSquadBonusDetails);
+      await show();
+    }
   }
 };
 </script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.battle-unit-list {
+  display: grid;
+  grid-template-columns: repeat(5, minmax(10rem, 1fr));
+  justify-items: center;
+  row-gap: 0.5rem;
+  column-gap: 0.5rem;
+}
+.battle-unit {
+  width: 100%;
+}
+</style>
---
+++ packages/client/src/views/Battle/BattleSquadBonusDetails.vue
@@ -0,0 +1,26 @@
+<template>
+  <UserDialog @close="$close" title="squad bonus">
+    <template v-slot:content>
+      <div class="font-size-22">
+        information.....
+      </div>
+    </template>
+    <template v-slot:footer>
+      <CustomButton type="yellow" @click="handleClose">{{
+        $t("close")
+      }}</CustomButton>
+    </template>
+  </UserDialog>
+</template>
+
+<script>
+export default {
+  props: ["usedUnits"],
+  data: () => ({}),
+  methods: {
+    handleClose() {
+      this.$close();
+    }
+  }
+};
+</script>
