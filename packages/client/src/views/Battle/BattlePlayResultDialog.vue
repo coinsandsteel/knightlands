0d92e1f0ff92398ca10a59bde9cc990f196a4156
Phuc Le <lehuuphucaptech@gmail.com>
2022-08-04 18:44:13 +0700

    chg: combat result dialog

--- packages/client/src/views/Battle/BattlePlayResultDialog.vue
+++ packages/client/src/views/Battle/BattlePlayResultDialog.vue
@@ -6,10 +6,42 @@
   >
     <template v-slot:content>
       <div class="text-align-center font-size-22">
-        <!-- <div>Rewards</div>
-        <div>Crystals: 57</div>
-        <div>Your rating: 14 (-3)</div> -->
-        <div>
+        <div class="">Rewards</div>
+        <div class="margin-top-2">
+          <div class="flex-inline">
+            Crystals:
+            <BattleCrystal class="margin-left-1 inline-block" :value="123" />
+          </div>
+        </div>
+        <div class="margin-top-1">
+          <div class="flex-inline">
+            Coins:
+            <BattleCoin
+              class="margin-left-1 inline-block"
+              :hasMargin="true"
+              :value="123"
+            />
+          </div>
+        </div>
+        <div class="margin-top-2 text-align-center">
+          <div v-for="(unit, index) in units" :key="index">
+            <div class="flex-inline flex-item-center">
+              <BattleUnit :unit="unit" />
+              <div class="flex-grow-1 margin-left-2">
+                <progress-bar
+                  ref="energy"
+                  :maxValue="456"
+                  :value="123"
+                  :hideMaxValue="true"
+                  :expand="false"
+                  barType="green"
+                  class="battle-unit-experience pointer"
+                ></progress-bar>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="text-align-center">
           <CustomButton
             type="yellow"
             class="inline-block margin-right-2 margin-top-4"
@@ -27,16 +59,25 @@
 <script>
 import { mapState } from "vuex";
 import BattleUnit from "@/views/Battle/BattleUnit.vue";
+import BattleCoin from "@/views/Battle/BattleCoin.vue";
+import BattleCrystal from "@/views/Battle/BattleCrystal.vue";
+import ProgressBar from "@/components/ProgressBar.vue";
 import * as battle from "@/../../knightlands-shared/battle";
 
 export default {
   components: {
-    BattleUnit
+    BattleUnit,
+    BattleCoin,
+    BattleCrystal,
+    ProgressBar
   },
   computed: {
     ...mapState("battle", ["game", "user"]),
     hasWon() {
       return this.game.combat.result === battle.COMBAT_RESULT_WIN;
+    },
+    units() {
+      return this.game.userSquad.units || [];
     }
   },
   methods: {
@@ -51,3 +92,11 @@ export default {
   }
 };
 </script>
+<style scoped lang="less">
+.battle-unit-experience {
+  width: 30rem;
+}
+.battle-unit {
+  width: 7rem;
+}
+</style>
