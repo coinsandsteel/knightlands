a2501ca458d75ae5cd0c619a80b858275caee6d1
Phuc Le <lehuuphucaptech@gmail.com>
2022-10-04 17:54:25 +0700

    chg: redirect to menu page if combat is not started

--- packages/client/src/views/Battle/BattleAdventurePlay.vue
+++ packages/client/src/views/Battle/BattleAdventurePlay.vue
@@ -1,16 +1,41 @@
 <template>
-  <div>
+  <div v-if="isActive && isStarted">
     <BattlePlayField />
   </div>
 </template>
 <script>
+import { mapState } from "vuex";
+import ActivityMixin from "@/components/ActivityMixin.vue";
 import BattlePlayField from "@/views/Battle/BattlePlayField.vue";
 export default {
+  mixins: [ActivityMixin],
   components: {
     BattlePlayField
   },
   data() {
     return {};
+  },
+  computed: {
+    ...mapState("battle", ["game", "user"]),
+    isStarted() {
+      return this.game.combat.started;
+    }
+    // isResultVisible() {
+    //   return !!this.game.combat.result;
+    // }
+  },
+  watch: {
+    isStarted(value, oldValue) {
+      if (this.isActive && oldValue === true && value === false) {
+        this.$router.replace({ name: "battle-menu" });
+      }
+    }
+  },
+  mounted() {
+    if (!this.isStarted) {
+      this.$router.replace({ name: "battle-menu" });
+      return;
+    }
   }
 };
 </script>
