7db8eb56df8e036b1b3b0f86c2a45b8bc572b4cc
Phuc Le <lehuuphucaptech@gmail.com>
2022-10-10 15:03:13 +0700

    chg: render squad bonus

--- packages/client/src/views/Battle/BattleSquad.vue
+++ packages/client/src/views/Battle/BattleSquad.vue
@@ -88,25 +88,203 @@ export default {
       }, 0);
     },
     bonuses() {
-      const bonusItems = (this.game && this.game.userSquad
+      let bonusItems = (this.game && this.game.userSquad
         ? this.game.userSquad.bonuses || []
         : []
       ).filter(({ type }) => type);
+
+      // bonusItems = [
+      //   {
+      //     target: "attack",
+      //     mode: "constant",
+      //     operation: "multiply",
+      //     value: 1.5
+      //   },
+      //   {
+      //     target: "speed",
+      //     mode: "constant",
+      //     trigger: "debuff",
+      //     operation: "multiply",
+      //     value: 4
+      //   },
+      //   {
+      //     target: "attack",
+      //     mode: "constant",
+      //     operation: "multiply",
+      //     value: 1.1
+      //   },
+      //   {
+      //     target: "attack",
+      //     mode: "stack",
+      //     delta: 0.025,
+      //     trigger: "damage",
+      //     multiply: true,
+      //     max: 0.15
+      //   }
+      // ];
 
       if (!bonusItems.length > 0) {
         return null;
       }
       return bonusItems
         .map(bonus => {
-          return bonus.type + (bonus.modifier ? " +" + bonus.modifier : "");
+          return this.renderBonus(bonus);
         })
-        .join(", ");
+        .filter(str => !!str)
+        .join(" / ");
     }
   },
   activated() {
     this.title = this.$t("Squad");
   },
   methods: {
+    renderBonus(bonus) {
+      let result = "";
+
+      if (
+        bonus.target === "attack" &&
+        bonus.mode === "constant" &&
+        bonus.operation === "multiply"
+      ) {
+        const value = Math.floor((bonus.value - 1) * 100);
+        result = `Attack ${value > 0 ? "+" : ""}${value}%`;
+      } else if (
+        bonus.target === "hp" &&
+        bonus.mode === "constant" &&
+        bonus.operation === "multiply"
+      ) {
+        const value = Math.floor((bonus.value - 1) * 100);
+        result = `HP ${value > 0 ? "+" : ""}${value}%`;
+      } else if (
+        bonus.target === "defence" &&
+        bonus.mode === "constant" &&
+        bonus.operation === "multiply"
+      ) {
+        const value = Math.floor((bonus.value - 1) * 100);
+        result = `Defense ${value > 0 ? "+" : ""}${value}%`;
+      } else if (
+        bonus.target === "abilities" &&
+        bonus.mode === "constant" &&
+        bonus.operation === "multiply"
+      ) {
+        const value = Math.floor((bonus.value - 1) * 100);
+        result = `Abilities power ${value > 0 ? "+" : ""}${value}%`;
+      } else if (
+        bonus.target === "speed" &&
+        bonus.mode === "constant" &&
+        bonus.terrain === "swamp"
+      ) {
+        if (bonus.scheme === "swamp-1") {
+          result = "Swamp 1 (-25%)";
+        } else if (bonus.scheme === "swamp-2") {
+          result = "Swamp 2 (-50%)";
+        } else if (bonus.scheme === "swamp-3") {
+          result = "Swamp 3 (-75%)";
+        } else if (bonus.scheme === "swamp-4") {
+          result = "Swamp 4 (-100%)";
+        }
+      } else if (
+        bonus.target === "power" &&
+        bonus.mode === "constant" &&
+        bonus.terrain === "hill"
+      ) {
+        if (bonus.scheme === "hill-1") {
+          result = "Hills 1 (+25%)";
+        } else if (bonus.scheme === "hill-2") {
+          result = "Hills 2 (+50%)";
+        } else if (bonus.scheme === "hill-3") {
+          result = "Hills 3 (+75%)";
+        } else if (bonus.scheme === "hill-4") {
+          result = "Hills 4 (+100%)";
+        }
+      } else if (
+        bonus.target === "defence" &&
+        bonus.mode === "constant" &&
+        bonus.terrain === "woods"
+      ) {
+        if (bonus.scheme === "woods-1") {
+          result = "Woods 1 (+25%)";
+        } else if (bonus.scheme === "woods-2") {
+          result = "Woods 2 (+50%)";
+        } else if (bonus.scheme === "woods-3") {
+          result = "Woods 3 (+75%)";
+        } else if (bonus.scheme === "woods-4") {
+          result = "Woods 4 (+100%)";
+        }
+      } else if (
+        bonus.target === "defence" &&
+        bonus.mode === "constant" &&
+        bonus.terrain === "ice"
+      ) {
+        if (bonus.scheme === "ice-1") {
+          result = "Ice 1 (-25%)";
+        } else if (bonus.scheme === "ice-2") {
+          result = "Ice 2 (-50%)";
+        } else if (bonus.scheme === "ice-3") {
+          result = "Ice 3 (-75%)";
+        } else if (bonus.scheme === "ice-4") {
+          result = "Ice 4 (-100%)";
+        }
+      } else if (
+        bonus.subEffect === "lava_damage" &&
+        bonus.mode === "constant" &&
+        bonus.terrain === "lava"
+      ) {
+        if (bonus.scheme === "lava-1") {
+          result = "Lava 1 (-25%)";
+        } else if (bonus.scheme === "lava-2") {
+          result = "Lava 2 (-50%)";
+        } else if (bonus.scheme === "lava-3") {
+          result = "Lava 3 (-75%)";
+        } else if (bonus.scheme === "lava-4") {
+          result = "Lava 4 (-100%)";
+        }
+      } else if (
+        bonus.target === "defence" &&
+        bonus.mode === "stack" &&
+        bonus.trigger === "damage" &&
+        bonus.operation === "add" &&
+        bonus.value &&
+        bonus.max === "add"
+      ) {
+        result = `When a squad member takes damage the squad's defense is increased by +${bonus.value} (max. ${bonus.max})`;
+      } else if (
+        bonus.subEffect === "counter_attack" &&
+        bonus.mode === "burst" &&
+        bonus.probability
+      ) {
+        result = `Chance of a counter attack ${bonus.probability}%`;
+      } else if (
+        bonus.target === "speed" &&
+        bonus.mode === "constant" &&
+        bonus.trigger === "debuff"
+      ) {
+        const value = bonus.value || bonus.delta;
+        result = `When a unit is debuffed, their speed is increased by +${value}`;
+      } else if (
+        bonus.target === "attack" &&
+        bonus.mode === "stack" &&
+        bonus.trigger === "damage" &&
+        bonus.multiply &&
+        bonus.delta &&
+        bonus.max
+      ) {
+        const delta = (bonus.delta * 100).toFixed(1);
+        const max = bonus.max * 100;
+        result = `When a squad member takes damage the squad's attack is increased by ${delta}% (max. ${max}%)`;
+      } else if (
+        bonus.target === "power" &&
+        bonus.mode === "burst" &&
+        bonus.operation === "multiply" &&
+        bonus.value &&
+        bonus.probability
+      ) {
+        const probability = bonus.probability * 100;
+        result = `Chance to deal a critical hit ${probability}% (damage x${bonus.value})`;
+      }
+
+      return result;
+    },
     buildSquad() {
       this.$store.dispatch("battle/testAction", {
         action: "buildSquad"
